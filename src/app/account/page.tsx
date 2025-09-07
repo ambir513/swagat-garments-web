8e14210f75219dc2770cb3f1c043d34a148c827b
Amar Biradar <ambir0513@gmail.com>
2025-09-07 23:14:31 +0530

    delete(folder): delete the folder and files

--- src/app/account/page.tsx
+++
@@ -1,5 +0,0 @@
-const page = () => {
-  return <div className="">Account</div>;
-};
-
-export default page;
--- src/app/auth/forgot-password/page.tsx
+++
@@ -1,5 +0,0 @@
-const page = () => {
-  return <div className="">Forgot Password</div>;
-};
-
-export default page;
